c629ca7bb3ebeb5c80af7dcba0816d8681aa4dd0
Laurent Tréguier <laurent.treguier@with-madrid.com>
2023-04-03 16:14:57 +0200

    Fix default consumer import

--- src/model_w/project_maker/template/api/___project_name__snake___/django/routing.py
+++ src/model_w/project_maker/template/api/___project_name__snake___/django/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 
-from ___project_name__snake___.core.consumers import PassiveAggressiveConsumer
+from ___project_name__snake___.apps.realtime.consumers import PassiveAggressiveConsumer
 
 websocket_urlpatterns = [
     re_path(r"back/ws/hello/$", PassiveAggressiveConsumer.as_asgi()),
