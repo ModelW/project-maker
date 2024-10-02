0ab6fce0da9e982663709e58e0583338a6fbbd48
Dave O'Neill <david.oneill@with-madrid.com>
2024-10-02 18:46:29 +0200

    Remove script for running in docker, and use makefile instead
    - not likely to be used by devs often, but useful for debugging GHA locally

--- src/model_w/project_maker/template/run-tests-in-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run tests in docker and pass through any arguments
-docker compose -f ./api/bdd/_infrastructure/docker-compose-e2e.yaml up --build --abort-on-container-exit --remove-orphans
-
-# Open the test report
-xdg-open ./api/bdd/report/results/report.html &> /dev/null
